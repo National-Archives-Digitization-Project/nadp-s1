6109fd0c18bba2b3000619205e594eb1bca50cf0
myreedbricks <reedbricks@gmail.com>
2022-08-04 13:25:35 +0100

    Error tracing and fixing

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,13 +3,12 @@ const router = express.Router()
 
 router.get('/', (req, res,) => {
     // if (!req.session.isLogggedIn) {
-    //     res.redirect('/')
+    //      res.redirect('/')
     // }
-    res.render("dashboard", {
+    res.render("../dashboard", {
         title: "Dashboard"
     })
 })
-
 
 router.get('/logout', (req, res,) => {
     req.session.isLogggedIn = false
