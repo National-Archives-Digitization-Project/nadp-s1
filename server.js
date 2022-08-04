c5adfdaac3e9065d9eff36035109e8fe9832d0d8
myreedbricks <reedbricks@gmail.com>
2022-08-04 12:19:39 +0100

    Testing production

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const server = express()
 require('dotenv').config()
 const path = require('path')
 // const ejs = require('ejs')
-const helmet = require('helmet')
+// const helmet = require('helmet')
 // const { session, sessionStore, connected } = require('./services/database')
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
-server.use(helmet())
+// server.use(helmet())
 // server.use(session({
 //     secret: 'NADP1',
 //     name: 'NADP1',
@@ -26,24 +26,25 @@ server.use(express.static(path.join(__dirname, 'views')))
 server.use(express.static(path.join(__dirname, 'views', 'assets')))
 
 server.get('/', (req, res) => {
+    res.send('Welcome')
     // console.log(session.connected)
-    res.render("home", {
-        title: "Welcome to NADP Server (I)"
-        // isConnected: session.connected
-    })
+    // res.render("home", {
+    //     title: "Welcome to NADP Server (I)"
+    //     // isConnected: session.connected
+    // })
 })
 
-const loginRoute = require('./routes/login')
-server.use('/forms', loginRoute)
+// const loginRoute = require('./routes/login')
+// server.use('/forms', loginRoute)
 
-const dashboardRoute = require('./routes/dashboard')
-server.use('/dashboard', dashboardRoute)
+// const dashboardRoute = require('./routes/dashboard')
+// server.use('/dashboard', dashboardRoute)
 
-server.get('*', (req, res) => {
-    res.render('404', {
-        title: "NADP (I) - Page not found"
-    })
-})
+// server.get('*', (req, res) => {
+//     res.render('404', {
+//         title: "NADP (I) - Page not found"
+//     })
+// })
 
 const port = process.env.PORT || 4000
 server.listen(port, () => {
