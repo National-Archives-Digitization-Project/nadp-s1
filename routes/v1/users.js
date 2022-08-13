eb235e6e89ec2663898a2c58c3376cabebc0ac25
myreedbricks <reedbricks@gmail.com>
2022-08-13 19:44:03 +0100

    Updated NUPO Banners

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -3,29 +3,27 @@ const router = express();
 const dbCon = require("../../models");
 const userExist = require("../../middlewares/userExist");
 const bcryptjs = require('bcryptjs');
-const secrete = process.env.SALT_SECRET || "delta1201"
-//Create new user
-router.post("/create", userExist, async (req, res) => {
+
+// Create new user
+// Error code 4
+router.post("/create", userExist, (req, res) => {
     const { surname, firstname, email, mobile, password } = req.body;
-    if (req.hasEmail) {
-        res.status(200).json({ status: 1, err: 10, errMsg: "User email or password exists", data: {} })
-        res.end();
+    if (!req.hasEmail) {
+        new dbCon.user({
+            surname: surname,
+            firstname: firstname,
+            email: email.toLowerCase(),
+            mobile: mobile,
+            password: bcryptjs.hashSync(password, 10)
+        }).save((err, user) => {
+            if (err) {
+                res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: user })
+            } else {
+                res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: user })
+            }
+        })
     }
-    await new dbCon.user({
-        surname: surname,
-        firstname: firstname,
-        email: email.toLowerCase(),
-        mobile: mobile,
-        password: bcryptjs.hashSync(password, 10)
-    }).save((err, user) => {
-        if (err) {
-            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: user })
-            res.end();
-        } else {
-            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: user })
-            res.end();
-        }
-    })
+    res.status(200).json({ status: 1, err: 40, errMsg: "User email or password exists", data: {} });
 })
 
 router.all("*", (req, res) => {
