97bbdf99860e4269446aee92af9cd310dd8fb60c
myreedbricks <reedbricks@gmail.com>
2022-08-14 10:06:29 +0100

    Settign the error standards

--- controlers/user.controler.js
+++ controlers/user.controler.js
@@ -1,11 +1,58 @@
 
 const dbCon = require("../models");
+const bcryptjs = require('bcryptjs');
 
-exports.findAll = async (req, res) => {
+exports.all = async (req, res) => {
     await dbCon.user.find({})
         .then((users) => {
             res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: users });
         }).catch((err) => {
             res.status(200).json({ status: 1, err: 40, errMsg: err, data: {} });
+        })
+}
+
+exports.info = async (req, res) => {
+    await dbCon.user.find({ _id: req.params.id })
+        .then((users) => {
+            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: users });
+        }).catch((err) => {
+            res.status(200).json({ status: 1, err: 40, errMsg: err, data: {} });
+        })
+}
+
+
+exports.create = async (req, res) => {
+    const { surname, firstname, email, mobile, password } = req.body;
+    if (!req.hasEmail) {
+        await new dbCon.user({
+            surname: surname,
+            firstname: firstname,
+            email: email.toLowerCase(),
+            mobile: mobile,
+            password: bcryptjs.hashSync(password, 10)
+        }).save((err, result) => {
+            if (err) {
+                res.status(500).json({ status: 1, err: 40, errMsg: err, data: {} });
+                return;
+            }
+            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: result });
+        })
+    } else {
+        res.status(200).json({ status: 1, err: 40, errMsg: "User email or password exists", data: {} });
+    }
+}
+
+
+exports.update = async (req, res) => {
+    const { surname, firstname, mobile } = req.body;
+    await dbCon.user.updateOne({ _id: req.params.id }, {
+        surname: surname,
+        firstname: firstname,
+        mobile: mobile
+    })
+        .then((user) => {
+            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: user });
+        }).catch((err) => {
+            res.status(200).json({ status: 1, err: 40, errMsg: err.errMsg, data: {} });
         })
 }
--- routes/v1/users.js
+++ routes/v1/users.js
@@ -2,63 +2,15 @@ const express = require('express');
 const router = express();
 const dbCon = require("../../models");
 const userExist = require("../../middlewares/userExist");
-const bcryptjs = require('bcryptjs');
 
-const { findAll } = require("../../controlers/user.controler")
+const { all, info, create, update } = require("../../controlers/user.controler")
 
 // Error code 4
 // Create new user
-
-router.get("/all", findAll);
-
-router.get("/:id/info", async (req, res) => {
-    await dbCon.user.find({ _id: req.params.id })
-        .then((users) => {
-            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: users });
-        }).catch((err) => {
-            res.status(200).json({ status: 1, err: 40, errMsg: err, data: {} });
-        })
-})
-
-
-router.post("/create", userExist, async (req, res) => {
-    const { surname, firstname, email, mobile, password } = req.body;
-    if (!req.hasEmail) {
-        await new dbCon.user({
-            surname: surname,
-            firstname: firstname,
-            email: email.toLowerCase(),
-            mobile: mobile,
-            password: bcryptjs.hashSync(password, 10)
-        }).save((err, result) => {
-            if (err) {
-                res.status(500).json({ status: 1, err: 40, errMsg: err, data: {} });
-                return;
-            }
-            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: result });
-        })
-    } else {
-        res.status(200).json({ status: 1, err: 40, errMsg: "User email or password exists", data: {} });
-    }
-})
-
-
-
-router.post("/:id/update", async (req, res) => {
-    const { surname, firstname, email, mobile, password } = req.body;
-
-
-    await dbCon.user.updateOne({ _id: req.params.id })
-        .then((users) => {
-            res.status(200).json({ status: 1, err: 0, errMsg: "Success", data: users });
-        }).catch((err) => {
-            res.status(200).json({ status: 1, err: 40, errMsg: err, data: {} });
-        })
-})
-
-
-
-
+router.get("/all", all);
+router.get("/:id/info", info)
+router.post("/create", userExist, create)
+router.post("/:id/update", update)
 
 router.all("*", (req, res) => {
     res.status(500).json({ status: 0, err: 0, errMsg: "Invalid User API endpoint", data: {} });
